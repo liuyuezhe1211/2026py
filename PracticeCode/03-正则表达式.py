6261f19993a5db62c9490a0c67ed6764f749afd2
liuyuezhe <liuyuezhe1211@163.com>
2026-01-22 15:18:57 +0800

    测试

---
+++ PracticeCode/03-正则表达式.py
@@ -0,0 +1,10 @@
+# 作者: 刘悦喆
+# 2026年01月22日15时12分29秒
+# liuyuezhe1211@163.com
+
+import re
+
+email_list = ["xiaoWang@163.com", "xiaoWang@163.comheihei", ".com.xiaowang@qq.com"]
+for ls in email_list:
+    res1=re.match(r"[0-9a-zA-Z]{4,20}@163\.com$",ls)
+    print(res1)
